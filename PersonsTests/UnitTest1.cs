1a460b3bb78396386f41e67f82cd8187a4216a3f
Boťa Jan <botajan@email.cz>
2021-02-10 00:09:32 +0100

    Základní nastavení testovacího prostředí.

--- PersonsTests/UnitTest1.cs
+++ PersonsTests/UnitTest1.cs
@@ -1,5 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GalaSoft.MvvmLight.Command;
 using Persons;
+using Persons.Model;
+using Persons.ViewModel;
 
 namespace PersonsTests
 {
@@ -9,6 +13,19 @@ namespace PersonsTests
         [TestMethod]
         public void TestMethod1()
         {
+           // // Arrange
+           // string testMessage = "Unit test zprava"; Persons.
+           // ZpravaModel z = ZpravaModel.ZpravaDatabase;
+           // ZpravaViewModel zvm = new ZpravaViewModel { Zprava = testMessage };
+           // 
+           // // Act 
+           // // Tohle za nás v aplikaci udělá binding
+           // // na TextBox a Button. V testu to dokážeme simulovat voláním.
+           // zvm.SendCommand.Execute(null);
+           //
+           // // Assert
+           // // Ověř, že se zpráva dostala do datového modelu
+           // Assert.AreEqual(z.VsechnyZpravy[0], testMessage);
         }
     }
 }
