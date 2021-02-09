d7fd7cbc1d774eba8d422998b66ae0b85cfb6fe7
Boťa Jan <botajan@email.cz>
2021-02-10 00:04:39 +0100

    Přidání testovacího prostředí.

---
+++ PersonsTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Persons;
+
+namespace PersonsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
